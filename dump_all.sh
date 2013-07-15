9ab096c89da80cbd892c22967672c73046e29794
Brad Pitcher <bradpitcher@gmail.com>
2013-07-14 23:28:15 -0700

    use scene file, add some comments

--- dump_all.sh
+++ dump_all.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # googlefontdirectory is a git submodule that needs to be initialized first.
+# This script also requires stl2pov. Find it here: http://rsmith.home.xs4all.nl/software/py-stl-stl2pov.html
+# And povray found in your friendly linux repository
 mkdir -p fonts # make sure output directory exists
 find ./googlefontdirectory/fonts/*.ttf | awk '{
   split($1, path, "/")
@@ -10,12 +12,12 @@ find ./googlefontdirectory/fonts/*.ttf | awk '{
   system(cmd)
 
   # Uncomment this block to generate *_test.stl and test png files for all fonts. Will take about a day
-  #system("echo \"include <" name ".scad>\n\" > " name "_test.scad")
+  system("echo \"include <fonts/" name ".scad>\n\" > " name "_test.scad")
   ##system("echo \"" name "(\\\"Grumpy wizards make toxic brew for the evil Queen and Jack.\\\");\" >> " name "_test.scad")
   #system("echo \"text = \\\"" name "\\\";\nwidth = " name "_width(text);\n" name "(text, center = true);\nlinear_extrude(height = 10)\ndifference() {\n\tsquare([width + 10, 0.75 * (width + 10)], center = true);\n\tsquare([width + 10 - 2, 0.75 * (width + 10 - 2)], center = true);\n}\" >> " name "_test.scad")
-  #system("openscad -m make " name "_test.scad -o " name "_test.stl")
-  #system("stl2pov " name "_test.stl > font.inc")
-  #system("povray + font.pov +O" name ".png +D +W640 +H480 +A0.5")
+  #system("openscad -m make " name "_test.scad -o fonts/" name ".stl")
+  #system("stl2pov fonts/" name ".stl > font.inc")
+  #system("povray +Iscene.pov +Ofonts/" name ".png +D +W640 +H480 +A0.5")
   #system("rm font.inc")
   #system("rm " name "_test.scad")
 
